7b595b433367cd83032d36724dfdceb456f33ba1
super <super@s4p3r.local>
2021-10-29 23:03:01 +0800

    fix

--- src/Frontend/src/api/index.js
+++
@@ -1,5 +0,0 @@
-import request from '@/utils/request';
-
-
-export const fetchLogin = (url, params) => request.post(url, params);
-export const fetchList = (url, params) => request.get(url, {params});
